3bd787947d15fafce400b365377621135025dfbb
Oleksandr <scriptchevy@gmail.com>
2019-08-09 12:03:26 +0300

    Add files via upload

---
+++ routes.js
@@ -0,0 +1,11 @@
+﻿const recipes = require('./handlers/home.js');
+const add = require('./handlers/addRecipe.js');
+const edit = require('./handlers/editRecipe.js');
+const history = require('./handlers/viewHistory.js');
+
+module.exports = function (app) {
+  app.get('/api/recipes', recipes.home);
+  app.post('/api/recipes', add.addRecipe);
+  app.put('/api/edit', edit.editRecipe);
+  app.post('/api/history', history.viewHistory);
+};
