f6746fc9a152de929816b97153b201a3bef48436
Oleksandr <scriptchevy@gmail.com>
2019-08-09 12:09:34 +0300

    Update index.js

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -8,119 +8,49 @@ export const recipesIsLoading = bool => ({
   recipesIsLoading: bool
 });
 
-export const recipesFetchDataSuccess = recipes => {
-  return {
-    type: 'RECIPES_FETCH_DATA_SUCCESS',
-    recipes
-  };
-}
+export const recipesFetchDataSuccess = recipes => ({
+  type: 'RECIPES_FETCH_DATA_SUCCESS',
+  recipes
+});
 
-export const recipesFetchData = url => {
-  return (dispatch) => {
-    dispatch(recipesIsLoading(true));
+export const getRecipes = () => ({
+  type: 'GET_RECIPES'
+});
 
-    fetch(url)
-      .then((res) => {
-        if (!res.ok) {
-          throw Error(res.statusText);
-        }
-        dispatch(recipesIsLoading(false));
-        return res;
-      })
-      .then((res) => res.json())
-      .then((result) => dispatch(recipesFetchDataSuccess(result.recipes)))
-      .catch(() => dispatch(recipesHasErrored(true)));
-  };
-}
+export const editRecipe = (recipeId, recipeName, recipeDescription) => ({
+  type: 'EDIT_RECIPE',
+  recipeId, recipeName, recipeDescription
+});
+
+export const addRecipe = (recipeName, recipeDescription) => ({
+  type: 'ADD_RECIPE',
+  recipeName, recipeDescription
+});
+
+export const getRecipeHistory = recipeId => ({
+  type: 'GET_RECIPE_HISTORY',
+  recipeId
+});
 
 export const setRecipeNameEdit = recipeName => ({
   type: 'SET_RECIPE_NAME_EDIT',
-  payload: recipeName
+  recipeName
 });
 
 export const setRecipeDescriptionEdit = recipeDescription => ({
   type: 'SET_RECIPE_DESCRIPTION_EDIT',
-  payload: recipeDescription
+  recipeDescription
 });
 
 export const setRecipeId = recipeId => ({
   type: 'SET_RECIPE_ID',
-  payload: recipeId
+  recipeId
 });
 
-export const addRecipe = (url, recipeName, recipeDescription) => {
-  return (dispatch) => {
-    fetch(url, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        recipeName: recipeName,
-        recipeDescription: recipeDescription
-      })
-    }).then((res) => res.json())
-      .then((result) => {
-        if (result.fieldError) {
-          alert(result.fieldError);
-          return;
-        }
-        dispatch(recipesFetchDataSuccess(result.recipes))
-      })
-      .catch(() => dispatch(recipesHasErrored(true)))
-  };
-}
-
-export const editRecipe = (url, recipeId, recipeName, recipeDescription) => {
-  return (dispatch) => {
-    fetch(url, {
-      method: 'PUT',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        recipeId: recipeId,
-        recipeName: recipeName,
-        recipeDescription: recipeDescription
-      })
-    }).then((res) => res.json())
-      .then((result) => {
-        if (result.fieldError) {
-          alert(result.fieldError);
-          return;
-        }
-        dispatch(recipesFetchDataSuccess(result.recipes))
-      })
-      .catch(() => dispatch(recipesHasErrored(true)))
-  };
-}
-
-export const recipeHistoryFetchSuccess = recipeHistory => {
-  return {
-    type: 'RECIPE_HISTORY_FETCH_SUCCESS',
-    recipeHistory
-  };
-}
-
-export const fetchRecipeHistory = (url, recipeId) => {
-  return (dispatch) => {
-    fetch(url, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ recipeId: recipeId })
-    }).then((res) => res.json())
-      .then((result) => {
-        if (result.fieldError) {
-          alert(result.fieldError);
-          return;
-        }
-        dispatch(recipeHistoryFetchSuccess(result.recipes))
-      })
-      .catch(() => dispatch(recipesHasErrored(true)))
-  };
-}
+export const recipeHistoryFetchSuccess = recipeHistory => ({
+  type: 'RECIPE_HISTORY_FETCH_SUCCESS',
+  recipeHistory
+});
 
 export const setHistoryVisibility = bool => ({
   type: 'SET_HISTORY_VISIBILITY',
